66e5f5f8eeec15e0d710445ff628f53df91ce057
romlopez1 <romibernard68@gmail.com>
2021-09-08 17:06:03 -0500

    se agregan imágenes de game over, winner y ajuste de fondo de canvas y html sin repetición

--- videojuego.js
+++ videojuego.js
@@ -102,7 +102,6 @@ class Components {
 //Motor del juego
 function updateGameArea () {
   myGameArea.clear(); //Limpia canvas
-  imgCanvasBack.update();//inserta imagen de inicio en el fondo
   drawLives(myGameArea.lives)//llama a función con # de vidas
   imgBasketObj.update();//inserta imagen a una frecuencia determinada
   imgBasketObj.newPos();//Actualiza la posición de la canasta
@@ -217,7 +216,6 @@ function applesCatches (arrApples, imgObj) {
 }
 
 //Inicialización de clase Constructor con imágenes
-const imgCanvasBack = new Components (0, 0, myGameArea.imgCanvasBack, 1200, 600)
 const imgBasketObj = new Components (500, 490, myGameArea.imgBasket, 100, 100);
 const imgOneAppleRed = new Components (0, 0, myGameArea.imgAppleRed, 80, 80);
 
